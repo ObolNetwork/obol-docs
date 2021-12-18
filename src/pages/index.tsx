df8e55321699ecbb255f0f55a27d671c4683b4a6
Oisín Kyne <oisin@obol.tech>
2021-12-18 19:15:29 +0000

    Update index title from text to svg including the logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,9 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        {/* Opt for SVG Obol title image rather than a text field */}
+        {/* <h1 className="hero__title">{siteConfig.title}</h1> */}
+        <img className={styles.heroLogo} alt={"Obol Network"} src={"/img/ObolHorizontalWhite.svg"} />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
