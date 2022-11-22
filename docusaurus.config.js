1e731f9b33c0856e38b941fe0895d8c2c079ccf7
f1lander <filanderuclez@gmail.com>
2022-11-21 22:17:00 -0600

    adds gtag to docs site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,42 +1,51 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/palenight'); 
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/palenight");
 // Override code blocks to be Obol coloured
-const customObolDarkTheme = { ...darkCodeTheme, plain: {color:"#bfc7d5",backgroundColor: "#243D42"}}
+const customObolDarkTheme = {
+  ...darkCodeTheme,
+  plain: { color: "#bfc7d5", backgroundColor: "#243D42" },
+};
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Obol Network',
-  tagline: 'Building Distributed Validators for Ethereum',
-  url: 'https://docs.obol.tech/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'ObolNetwork', // Usually your GitHub org/user name.
-  projectName: 'obol-docs', // Usually your repo name.
+  title: "Obol Network",
+  tagline: "Building Distributed Validators for Ethereum",
+  url: "https://docs.obol.tech/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "ObolNetwork", // Usually your GitHub org/user name.
+  projectName: "obol-docs", // Usually your repo name.
   trailingSlash: false,
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/ObolNetwork/obol-docs/edit/main/',
+          editUrl: "https://github.com/ObolNetwork/obol-docs/edit/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/ObolNetwork/obol-docs/edit/main/blog/',
+          editUrl: "https://github.com/ObolNetwork/obol-docs/edit/main/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
+    ],
+    [
+      "@docusaurus/plugin-google-gtag",
+      {
+        trackingID: "GTM-NZ2HGMB",
+        anonymizeIP: true,
+      },
     ],
   ],
 
@@ -44,87 +53,87 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: "dark",
         disableSwitch: false,
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'Obol Network',
+        title: "Obol Network",
         logo: {
-          alt: 'Obol Network Logo',
-          src: '/img/ObolIconDark.svg',
-          srcDark: '/img/ObolIconLight.svg'
+          alt: "Obol Network Logo",
+          src: "/img/ObolIconDark.svg",
+          srcDark: "/img/ObolIconLight.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Docs",
           },
-          {to: 'https://blog.obol.tech', label: 'Blog', position: 'left'},
+          { to: "https://blog.obol.tech", label: "Blog", position: "left" },
           {
-            href: 'https://github.com/obolnetwork/obol-docs',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/obolnetwork/obol-docs",
+            label: "GitHub",
+            position: "right",
           },
           {
-            type: 'docsVersionDropdown', 
-            position: 'right'
+            type: "docsVersionDropdown",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         logo: {
           alt: "Obol Network Logo",
           src: "/img/ObolLineWhite.svg",
           srcDark: "/img/ObolLineWhite.svg",
           width: "30px",
           height: "30px",
-          href: "https://obol.tech/"
+          href: "https://obol.tech/",
         },
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Intro to Obol',
-                to: '/docs/intro',
+                label: "Intro to Obol",
+                to: "/docs/intro",
               },
               {
-                label: 'Charon',
-                to: '/docs/dv/introducing-charon',
+                label: "Charon",
+                to: "/docs/dv/introducing-charon",
               },
               {
-                label: 'DV Launchpad',
-                to: '/docs/dvk/distributed-validator-keys',
+                label: "DV Launchpad",
+                to: "/docs/dvk/distributed-validator-keys",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/n6ebKsX46w',
+                label: "Discord",
+                href: "https://discord.gg/n6ebKsX46w",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/obolnetwork',
+                label: "Twitter",
+                href: "https://twitter.com/obolnetwork",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                href: 'https://blog.obol.tech/',
+                label: "Blog",
+                href: "https://blog.obol.tech/",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/obolnetwork/',
+                label: "GitHub",
+                href: "https://github.com/obolnetwork/",
               },
             ],
           },
@@ -134,13 +143,13 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: customObolDarkTheme,
-        additionalLanguages: ['solidity', 'json5'],
+        additionalLanguages: ["solidity", "json5"],
       },
       metadata: [
-        {property: "og:image", content: "/img/obol_og_image.png"},
-        {property: "og:image:width", content: "1200"},
-        {property: "og:image:height", content: "630"}
-      ]
+        { property: "og:image", content: "/img/obol_og_image.png" },
+        { property: "og:image:width", content: "1200" },
+        { property: "og:image:height", content: "630" },
+      ],
     }),
 };
 
