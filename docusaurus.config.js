58f457afdd8fc2c56427f7e651bc905b6b1a6850
Oisín Kyne <oisin@obol.tech>
2024-10-28 18:58:22 +0000

    Missed change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,4 +1,4 @@
-// @ts-nocheck
+// @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 const {themes} = require('prism-react-renderer');
 const lightCodeTheme = themes.github
